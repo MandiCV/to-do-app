8ea5bdf9956c81479cc8315fcbcddea975e8f70d
MandiCV <amandacascales@gmail.com>
2022-04-16 00:54:45 +0200

    Aplicación ToDo - Primera versión sin autenticación

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { map } from 'rxjs/operators';
+
+import { Task } from '../Task';
+
+
+@Injectable()
 export class TaskService {
+  apiRest = 'https://localhost:3000/api/tareas';
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+  }
+  getTasks() {
+    return this.http.get<Task[]>(`${this.apiRest}`)
+      .pipe(map(res => res));
+  }
+
+  addTask(newTask: Task) {
+    return this.http.post<Task>(`${this.apiRest}`, newTask)
+      .pipe(map(res => res));
+  }
+
+  deleteTask(id: any) {
+    return this.http.delete<any>(`${this.apiRest}/${id}`)
+      .pipe(map(res => res));
+  }
+
+  updateTask(id: any, newTaskData: Task) {
+    return this.http.put<any>(`${this.apiRest}/${id}`, newTaskData)
+      .pipe(map(res => res));
+  }
 }
